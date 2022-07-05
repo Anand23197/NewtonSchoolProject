bf91969e0f6dfe9f8ea657e7ccb374d74eb089d2
ANAND VISHWAKARMA <anand23197@gmail.com>
2022-07-05 06:48:12 +0530

    third

--- src/karomi/countOfWord/CountSameWord.java
+++ src/karomi/countOfWord/CountSameWord.java
@@ -7,9 +7,11 @@ public class CountSameWord {
         String[] st1 = {"apple", "fruit", "text", "world"};
         String[] st2 = {"text", "sam", "try", "apple", "world"};
 
+        //create two hash map data structure
         HashMap<String, Integer> tm1 = new HashMap<>();
         HashMap<String, Integer> tm2 = new HashMap<>();
 
+        //count the frequency of word in each string array
         for(int i = 0; i< st1.length; i++){
             String str = st1[i];
             tm1.put(str, tm1.getOrDefault(str, 0)+1);
@@ -19,6 +21,10 @@ public class CountSameWord {
             String str = st2[i];
             tm2.put(str, tm2.getOrDefault(str, 0)+1);
         }
+
+        //we will iterate through any of the two hash map and check the
+        //key in another hash map. If key is present add the value of key
+        // and add in result;
         int res = 0;
         for(Map.Entry<String, Integer> entry : tm1.entrySet()){
             if(tm2.containsKey(entry.getKey())){
