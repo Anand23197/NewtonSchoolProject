db6cc4cf64c7af8afe0ef6849c34a8aa2b977c92
Anand Vishwakarma <anand23197@gmail.com>
2024-01-18 21:45:53 +0530

    changes

--- src/playground/Main.java
+++ src/playground/Main.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.*;
 
 public class Main {
-    public static void main (String[] args) throws IOException {
-
+    public static void main(String[] args) {
+        
     }
 }
