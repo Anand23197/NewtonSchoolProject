78ed4b780962a50f5bfc4116b7579dd13e901077
anand.v <e@gm.com>
2024-04-08 16:51:16 +0530

    CHANGES

---
+++ src/adancedDSA/Graphs/dijkstra/DijkstraWithPq.java
@@ -0,0 +1,95 @@
+package adancedDSA.Graphs.dijkstra;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+class Pair{
+    int w;
+    int n;
+    Pair(int w, int n){
+        this.w = w;
+        this.n = n;
+    }
+}
+
+public class DijkstraWithPq {
+    public static void main(String[] args) {
+        int V = 3, E=3,S=2;
+        ArrayList<Integer> node1 = new ArrayList<Integer>() {{add(1);add(1);}};
+        ArrayList<Integer> node2 = new ArrayList<Integer>() {{add(2);add(6);}};
+        ArrayList<Integer> node3 = new ArrayList<Integer>() {{add(2);add(3);}};
+        ArrayList<Integer> node4 = new ArrayList<Integer>() {{add(0);add(1);}};
+        ArrayList<Integer> node5 = new ArrayList<Integer>() {{add(1);add(3);}};
+        ArrayList<Integer> node6 = new ArrayList<Integer>() {{add(0);add(6);}};
+
+        ArrayList<ArrayList<Integer>> inter1 = new ArrayList<ArrayList<Integer>>(){
+            {
+                add(node1);
+                add(node2);
+            }
+        };
+        ArrayList<ArrayList<Integer>> inter2 = new ArrayList<ArrayList<Integer>>(){
+            {
+                add(node3);
+                add(node4);
+            }
+        };
+        ArrayList<ArrayList<Integer>> inter3 = new ArrayList<ArrayList<Integer>>(){
+            {
+                add(node5);
+                add(node6);
+            }
+        };
+        ArrayList<ArrayList<ArrayList<Integer>>> adj= new ArrayList<ArrayList<ArrayList<Integer>>>(){
+            {
+                add(inter1); // for 1st node
+                add(inter2); // for 2nd node
+                add(inter3); // for 3rd node
+            }
+        };
+        //add final values of adj here.
+
+        int[] res= dijkstra(V,adj,S);
+
+        for(int i=0;i<V;i++){
+            System.out.print(res[i]+" ");
+        }
+        System.out.println();
+    }
+
+    static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S)
+    {
+        // Write your code here
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.w, b.w));
+
+        pq.add(new Pair(0, S));
+
+        int[] res = new int[V];
+
+        for(int i = 0;i<V;i++){
+            res[i] = (int)(1e9);
+        }
+        res[S] = 0;
+
+        while(!pq.isEmpty()){
+            int w = pq.peek().w;
+            int n = pq.peek().n;
+
+            pq.remove();
+
+            for(ArrayList<Integer> newAdj : adj.get(n)){
+                int adjw = newAdj.get(1);
+                int adjn = newAdj.get(0);
+
+                if(w + adjw < res[adjn]){
+                    res[adjn] = w+ adjw;
+                    pq.add(new Pair(res[adjn], adjn));
+                }
+            }
+
+        }
+
+        return res;
+
+    }
+}
---
+++ src/adancedDSA/Graphs/wordLadder2/WordLadder2.java
@@ -0,0 +1,37 @@
+package adancedDSA.Graphs.wordLadder2;
+
+import java.util.*;
+
+public class WordLadder2 {
+    public static void main(String[] args) {
+        String startWord = "der", targetWord = "dfs";
+        String[] wordList = {
+                "des",
+                "der",
+                "dfr",
+                "dgt",
+                "dfs"
+        };
+
+//      ArrayList<ArrayList<String>> ans = findSequences(startWord, targetWord, wordList);
+    }
+
+//    static ArrayList<ArrayList<String>> findSequences(String startWord, String targetWord, String[] wordList){
+
+//        Set<String> st = new HashSet<>();
+//        int len =  wordList.length;
+//
+//        for(int i = 0;i<len;i++){
+//            st.add(wordList[i]);
+//        }
+//
+//        Queue<ArrayList<String>> queue = new LinkedList<>();
+//        ArrayList<String> ls = new ArrayList<>();
+//
+//        ls.add(startWord);
+//        queue.add(ls);
+
+
+
+//    }
+}
